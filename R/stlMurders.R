9e48d082d96ff5702e312630bde50c88b76d012a
Christopher Prener <prenercg@slu.edu>
2017-03-17 13:18:33 -0500

    update documentation for the stlMurders script

--- R/stlMurders.R
+++ R/stlMurders.R
@@ -2,10 +2,18 @@
 #'
 #' A dataset containing murders for the years 2008 through 2016 in the City of St. Louis.
 #'
-#' @format A data frame with 848 rows and 30 variables:
+#' @format A data frame with 1394 rows and 10 variables:
 #' \describe{
-#'   \item{id}{USDOE identification number}
-#'   \item{brand}{vehicle brand}
+#'   \item{id}{STLMPD record number}
+#'   \item{dateoccurred}{string date in dmy format for when murder occurred}
+#'   \item{yearoccurred}{year murder occurred}
+#'   \item{monthoccurred}{month murder occurred}
+#'   \item{timeoccurred}{time murder occurred in 24-hour clock}
+#'   \item{description}{\code{HOMICIDE}}
+#'   \item{district}{SLMPD district where murder occurred}
+#'   \item{neighborhood}{neighborhood where murder occurred}
+#'   \item{ileadsaddress}{house address where murder occurred}
+#'   \item{ileadsstreet}{street where murer occurred}
 #'   ...
 #' }
 #' @source \url{http://www.slmpd.org/Crimereports.shtml}
