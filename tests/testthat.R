0babe9d0d8e62bef9b02db8aed453bbe0253e29d
Christopher Prener <prenercg@slu.edu>
2017-03-17 13:32:12 -0500

    unit test package

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(stldata)
+
+test_check("stldata")
---
+++ tests/testthat/test_stlMurders.R
@@ -0,0 +1,7 @@
+context("Data check")
+
+test_that("data objects are unchanged", {
+
+  expect_equal_to_reference(stlMurders, "stlMurders.rds")
+
+})
