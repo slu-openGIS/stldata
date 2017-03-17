b741d63113832205023a85e3d61e832bf2eb8f1b
Christopher Prener <prenercg@slu.edu>
2017-03-17 13:08:51 -0500

    Add function to open murder data

---
+++ R/stlMurders.R
@@ -0,0 +1,13 @@
+#' City of St. Louis Murders, 2008-2016
+#'
+#' A dataset containing murders for the years 2008 through 2016 in the City of St. Louis.
+#'
+#' @format A data frame with 848 rows and 30 variables:
+#' \describe{
+#'   \item{id}{USDOE identification number}
+#'   \item{brand}{vehicle brand}
+#'   ...
+#' }
+#' @source \url{http://www.slmpd.org/Crimereports.shtml}
+#'
+"stlMurders"
