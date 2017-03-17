fc59082fbdf9e5eb46fbfc98ca62202c723b2d46
Christopher Prener <prenercg@slu.edu>
2017-03-17 13:34:01 -0500

    add package-level data

---
+++ R/stldata-package.r
@@ -0,0 +1,9 @@
+#' stldata: City of St. Louis Data
+#'
+#' This package contains various datasets representing the City of St. Louis.
+#' These datasets are primary designed for teaching statistics, data science, and
+#' spatial data analysis using R.
+#'
+#' @name stldata
+#' @docType package
+NULL
