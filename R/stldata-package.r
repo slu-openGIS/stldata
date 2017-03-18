bd244b2dec15705af42a1f84eacb71c7e4886037
Christopher Prener <prenercg@slu.edu>
2017-03-18 10:10:13 -0500

    Update DESCRIPTION and package information in response to R CMD check

--- R/stldata-package.r
+++ R/stldata-package.r
@@ -1,8 +1,10 @@
-#' stldata: City of St. Louis Data
+#' City of St. Louis Data
 #'
 #' This package contains various datasets representing the City of St. Louis.
 #' These datasets are primary designed for teaching statistics, data science, and
-#' spatial data analysis using R.
+#' spatial data analysis using R. It currently contains two datasets describing murders
+#' in the City as well as blood lead level test results among children. If you have additional
+#' data you would like to donate to the package, please contact Chris at prenercg@@slu.edu.
 #'
 #' @name stldata
 #' @docType package
