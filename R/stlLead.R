eca7ecf7ff90db729766cfd82b058fd3e65471d2
Christopher Prener <prenercg@slu.edu>
2017-03-18 13:52:55 -0500

    Add examples for lead level data

--- R/stlLead.R
+++ R/stlLead.R
@@ -4,6 +4,10 @@
 #' the City of St. Louis by Census Tract. All demographic data are from the 2015 5-year
 #' American Community Survey estimates for St. Louis Census Tracts.
 #'
+#' @docType data
+#'
+#' @usage data(stlLead)
+#'
 #' @format A data frame with 106 rows and 15 variables:
 #' \describe{
 #'   \item{geoID}{full census tract id number}
@@ -21,8 +25,33 @@
 #'   \item{povertyTot_MOE}{margin of error for \code{povertyTot}}
 #'   \item{povertyU18}{estimate for children living below poverty line, 2015}
 #'   \item{povertyU18_MOE}{margin of error for \code{povertyU18}}
-#'   ...
 #' }
-#' @source \url{http://www.reuters.com/investigates/special-report/usa-lead-testing/#interactive-lead}
+#' @source \href{http://www.reuters.com/investigates/special-report/usa-lead-testing/#interactive-lead}{Reuters reporting on lead exposure} and 2015 5-year American Community Survey estimates for City of St. Louis Census Tracts via \href{https://factfinder.census.gov}{American Fact Finder}
+#' @references Pell, M.B. & Schneyer, J. (2016, December 19) Off the Charts: The thousands of U.S. locales where lead poisoning is worse than in Flint. \emph{Reuters}, retrieved from \href{http://www.reuters.com/investigates/special-report/usa-lead-testing}{article website}.
+#'
+#' @examples
+#' str(stlLead)
+#' head(stlLead)
+#' summary(stlLead$pctElevated)
+#' summary(stlLead$black)
+#' summary(stlLead$povertyTot)
+#'
+#' if (require("dplyr") & require("ggplot2")) {
+#'
+#'   # what is the relationship between race and lead test results?
+#'     stlLead %>%
+#'       mutate(propBlack = black / totalPop) %>%
+#'       ggplot(aes(x = pctElevated, y = propBlack)) +
+#'         geom_point() +
+#'         geom_smooth()
+#'
+#'   # relationship between poverty and lead test results?
+#'     stlLead %>%
+#'       mutate(propPvty = povertyTot / totalPop) %>%
+#'       ggplot(aes(x = pctElevated, y = propPvty)) +
+#'         geom_point(method=lm) +
+#'         geom_smooth()
+#'
+#' }
 #'
 "stlLead"
