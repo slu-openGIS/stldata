0997bd969a044a611bcc605014d58f2c0e2d9865
Christopher Prener <prenercg@slu.edu>
2017-03-18 14:06:27 -0500

    Update murder date with simplier variable names

--- inst/extdata/stlMurders.R
+++ inst/extdata/stlMurders.R
@@ -3,21 +3,34 @@ library(dplyr)
 
 # import full dataset of 2008-2016 St. Louis Crimes
 #
-#rraw data are available from the SLMPD - http://www.slmpd.org/Crimereports.shtml;
+# raw data are available from the SLMPD - http://www.slmpd.org/Crimereports.shtml;
 # data have been significantly cleaned and represent crimes that have valid spatial data;
 # cleaned data are too large to post here; contact Chris at prenercg@slu.edu if you are
 # interested in the larger dataset
 
-allCrime <- read.csv("crimeData.csv", stringsAsFactors = FALSE)
+allCrime <- read.csv("inst/extdata/crimeData.csv", stringsAsFactors = FALSE)
 
 # select murders from dataset, arrange by date and time, and drop unneeded variables
 
 stlMurders <- allCrime %>%
   filter(ucr == 1) %>%
   arrange(yearoccurred, monthoccurred, dayoccurred, timeoccurred) %>%
-  select(id, dateoccurred, yearoccurred, monthoccurred, timeoccurred,
-         description, district:ileadsstreet)
+  select(id, dateoccurred, yearoccurred, monthoccurred, dayoccurred, timeoccurred,
+         description, district, neighborhood, ileadsaddress, ileadsstreet) %>%
+  rename(fullDate = dateoccurred) %>%
+  rename(year = yearoccurred) %>%
+  rename(month = monthoccurred) %>%
+  rename(day = dayoccurred) %>%
+  rename(time = timeoccurred) %>%
+  rename(descrip = description) %>%
+  rename(nhood = neighborhood) %>%
+  rename(address = ileadsaddress) %>%
+  rename(street = ileadsstreet)
 
-# save as .RData file for inclusion in atlas week package
+# save as .RData file for inclusion in stldata package
 
-save(stlMurders, file = "stlMurders.RData")
+save(stlMurders, file = "data/stlMurders.RData")
+
+# save as .csv file for inclusion in stldata package
+
+write.csv(stlMurders, file = "inst/extdata/stlMurders.csv")
