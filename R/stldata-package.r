5465bd80284958755911c16236c809cd040c52a1
Christopher Prener <prenercg@slu.edu>
2017-03-18 12:34:20 -0500

    Update package documentation

--- R/stldata-package.r
+++ R/stldata-package.r
@@ -4,7 +4,9 @@
 #' These datasets are primary designed for teaching statistics, data science, and
 #' spatial data analysis using R. It currently contains two datasets describing murders
 #' in the City as well as blood lead level test results among children. If you have additional
-#' data you would like to donate to the package, please contact Chris at prenercg@@slu.edu.
+#' data you would like to donate to the package or variables you would like to see added to
+#' a table, please contact Chris by opening an issue on the package's Github page
+#' (\url{https://github.com/chris-prener/stldata/issues}).
 #'
 #' @name stldata
 #' @docType package
