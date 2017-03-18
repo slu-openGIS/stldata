f8cce1ebed1dea9c320294751823a610664307b1
Christopher Prener <prenercg@slu.edu>
2017-03-17 20:30:28 -0500

    Update test script

--- tests/testthat/test_stlMurders.R
+++ tests/testthat/test_stlMurders.R
@@ -2,6 +2,7 @@ context("Data check")
 
 test_that("data objects are unchanged", {
 
+  expect_equal_to_reference(stlLead, "stlLead.rds")
   expect_equal_to_reference(stlMurders, "stlMurders.rds")
 
 })
