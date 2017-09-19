963274511c255a06ccb2ac601c96aaa848c69486
Christopher Prener <chris.prener@slu.edu>
2017-09-19 06:52:02 -0500

    add new data to testthat routine

--- tests/testthat/test_stlData.R
+++ tests/testthat/test_stlData.R
@@ -1,8 +1,9 @@
 context("Data check")
 
 test_that("data objects are unchanged", {
-
+  expect_equal_to_reference(stlAsthma, "stlAsthma.rds")
+  expect_equal_to_reference(stlInsurance, "stlInsurance.rds")
   expect_equal_to_reference(stlLead, "stlLead.rds")
   expect_equal_to_reference(stlMurders, "stlMurders.rds")
-
+  expect_equal_to_reference(stlSmoking, "stlSmoking.rds")
 })
