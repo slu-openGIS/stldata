f7fa047485ba5c62074352507906fe9a2a586dad
Christopher Prener <chris.prener@slu.edu>
2017-09-18 21:46:14 -0500

    update pkg spelling

---
+++ R/stlData-package.R
@@ -0,0 +1,13 @@
+#' City of St. Louis Data
+#'
+#' This package contains various datasets representing various phenomena in the City of St. Louis.
+#' These datasets are primary designed for teaching statistics, data science, and
+#' spatial data analysis using R. It currently contains two datasets describing murders
+#' in the City as well as blood lead level test results among children. If you have additional
+#' data you would like to donate to the package or variables you would like to see added to
+#' a table, please contact Chris by opening an issue on the package's
+#' \href{https://github.com/chris-prener/stldata/issues}{Github page}.
+#'
+#' @name stlData
+#' @docType package
+NULL
