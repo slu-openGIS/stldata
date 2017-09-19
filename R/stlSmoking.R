8a85cac54f13a6f0c4f92d67f355f3726ef66d6c
Christopher Prener <chris.prener@slu.edu>
2017-09-19 06:48:47 -0500

    add script with documentation for smoking

---
+++ R/stlSmoking.R
@@ -0,0 +1,26 @@
+#' Crude Prevalance of Current Smoking, 2014
+#'
+#' A data set containing the estimated prevalance of current smoking among City of St. Louis residents by Census Tract.
+#'
+#' @docType data
+#'
+#' @usage data(stlSmoking)
+#'
+#' @format A data frame with 106 rows and 7 variables:
+#' \describe{
+#'   \item{geoID}{full census tract id number}
+#'   \item{tractCE}{census tract id number}
+#'   \item{nameLSAD}{census tract string}
+#'   \item{pctSmoke}{crude prevalance estimate of current no health insurance, percent}
+#'   \item{pctSmoke_Low}{lower bound of crude prevalance estimate}
+#'   \item{pctSmoke_High}{upper bound of crude prevalance estimate}
+#'   }
+#'
+#' @source \href{https://www.cdc.gov/500cities/}{Centers for Disease Control 500 Cities Project}
+#'
+#' #' @examples
+#' str(stlSmoking)
+#' head(stlSmoking)
+#' summary(stlSmoking$pctSmoke)
+#'
+"stlSmoking"
