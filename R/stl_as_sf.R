68c3975bb6385f61b690d27960c7bd1985c105ec
Christopher Prener <chris.prener@slu.edu>
2018-02-18 20:19:06 -0600

    add sf conversion function

---
+++ R/stl_as_sf.R
@@ -0,0 +1,44 @@
+#' Load tibble as sf
+#'
+#' @description For specified tibbles, this function will load them as simple features with appropriate
+#'     geometric data.
+#'
+#' @details This function currently works with the tibbles that contain census tract level data:
+#'     stl_tbl_asthma, stl_tbl_income, stl_tbl_insurance, stl_tbl_lead, and stl_tbl_smoking
+#'
+#' @param table One of the accepted tables
+#'
+#' @return A simple features object with the specified tibble joined to the appropriate geometry.
+#'
+#' @import sf
+#' @importFrom dplyr %>%
+#' @importFrom dplyr left_join
+#' @importFrom dplyr select
+#'
+#' @export
+stl_as_sf <- function(table) {
+
+  # quote input
+  tblQ <- rlang::quo_name(rlang::enquo(table))
+
+  # create list of acceptable tables
+  tableList <- c("stl_tbl_asthma", "stl_tbl_income", "stl_tbl_insurance",
+                 "stl_tbl_lead", "stl_tbl_smoking")
+
+  # compare input with list of tables
+  if (!!tblQ %nin% tableList) {
+    stop("The given table is not available for automatic conversion to an sf object.")
+  }
+
+  # merge data
+  merge <- left_join(stl_sf_tracts, table, by = c("GEOID" = "geoID"))
+
+  # clean up merge
+  merge %>%
+    select(-STATEFP, -COUNTYFP, -TRACTCE, -NAME, -NAMELSAD, -MTFCC, -FUNCSTAT, -AWATER,
+           -INTPTLAT, -INTPTLON) %>%
+    rename(geoID = GEOID) -> merge
+
+  # return output
+  return(merge)
+}
