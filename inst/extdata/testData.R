02bf082689bd69a9192b72f3aef873d75515653c
Christopher Prener <chris.prener@slu.edu>
2018-02-18 19:21:39 -0600

    rebuild unit tests for data

---
+++ inst/extdata/testData.R
@@ -0,0 +1,13 @@
+
+saveRDS(stl_sf_boundary, "tests/testthat/stl_sf_boundary.rds")
+saveRDS(stl_sf_historic, "tests/testthat/stl_sf_historic.rds")
+saveRDS(stl_sf_hydro, "tests/testthat/stl_sf_hydro.rds")
+saveRDS(stl_sf_tracts, "tests/testthat/stl_sf_tracts.rds")
+
+saveRDS(stl_tbl_asthma, "tests/testthat/stl_tbl_asthma.rds")
+saveRDS(stl_tbl_income, "tests/testthat/stl_tbl_income.rds")
+saveRDS(stl_tbl_insurance, "tests/testthat/stl_tbl_insurance.rds")
+saveRDS(stl_tbl_lead, "tests/testthat/stl_tbl_lead.rds")
+saveRDS(stl_tbl_murders, "tests/testthat/stl_tbl_murders.rds")
+saveRDS(stl_tbl_sluPlaces, "tests/testthat/stl_tbl_sluPlaces.rds")
+saveRDS(stl_tbl_smoking, "tests/testthat/stl_tbl_smoking.rds")
---
+++ tests/testthat/test_data.R
@@ -0,0 +1,15 @@
+context("Data check")
+
+test_that("data objects are unchanged", {
+  expect_equal_to_reference(stl_sf_boundary, "data/stl_sf_boundary.rds")
+  expect_equal_to_reference(stl_sf_historic, "data/stl_sf_historic.rds")
+  expect_equal_to_reference(stl_sf_hydro, "data/stl_sf_hydro.rds")
+  expect_equal_to_reference(stl_sf_tracts, "data/stl_sf_tracts.rds")
+  expect_equal_to_reference(stl_tbl_asthma, "data/stl_tbl_asthma.rds")
+  expect_equal_to_reference(stl_tbl_income, "data/stl_tbl_income.rds")
+  expect_equal_to_reference(stl_tbl_insurance, "data/stl_tbl_insurance.rds")
+  expect_equal_to_reference(stl_tbl_lead, "data/stl_tbl_lead.rds")
+  expect_equal_to_reference(stl_tbl_murders, "data/stl_tbl_murders.rds")
+  expect_equal_to_reference(stl_tbl_sluPlaces, "data/stl_tbl_sluPlaces.rds")
+  expect_equal_to_reference(stl_tbl_smoking, "data/stl_tbl_smoking.rds")
+})
