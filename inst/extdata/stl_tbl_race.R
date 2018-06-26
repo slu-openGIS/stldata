70aea71f51d3f95647a117d950d2a0aa2375c900
Christopher Prener, PhD <prenercg@slu.edu>
2018-06-26 16:18:39 -0500

    create race data set

---
+++ inst/extdata/stl_tbl_race.R
@@ -0,0 +1,37 @@
+library(tidycensus)
+library(dplyr)
+library(stringr)
+
+race2010 <- get_acs(geography = "tract", table = "B02001", year = 2010, state = 29, county = 510, output = "wide")
+race2016 <- get_acs(geography = "tract", table = "B02001", year = 2016, state = 29, county = 510, output = "wide")
+
+pop2010 <- select(race2010, GEOID, B02001_001E, B02001_001M)
+pop2016 <- select(race2016, GEOID, B02001_001E, B02001_001M)
+
+race2010 %>%
+  rename(white10 = B02001_002E) %>%
+  rename(white10_moe = B02001_002M) %>%
+  rename(black10 = B02001_003E) %>%
+  rename(black10_moe = B02001_003M) %>%
+  select(GEOID, NAME, white10, white10_moe, black10, black10_moe) -> race2010
+
+race2016 %>%
+  rename(white16 = B02001_002E) %>%
+  rename(white16_moe = B02001_002M) %>%
+  rename(black16 = B02001_003E) %>%
+  rename(black16_moe = B02001_003M) %>%
+  select(GEOID, white16, white16_moe, black16, black16_moe) -> race2016
+
+stl_tbl_race <- left_join(race2010, race2016, by = "GEOID")
+
+stl_tbl_race %>%
+  mutate(white_delta = white16-white10) %>%
+  mutate(black_delta = black16-black10) %>%
+  rename(geoID = GEOID) %>%
+  rename(nameLSAD = NAME) %>%
+  mutate(tractCE = as.integer(str_sub(geoID, start = -6, end = -1))) %>%
+  select(geoID, tractCE, nameLSAD, white10, white10_moe, white16, white16_moe, white_delta,
+         black10, black10_moe, black16, black16_moe, black_delta) -> stl_tbl_race
+
+save(stl_tbl_income, file = "data/stl_tbl_race.rda")
+saveRDS(stl_tbl_income, "tests/testthat/data/stl_tbl_race.rds")
